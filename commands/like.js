54a158c824c4ca12a3e02151a8438a9a85528276
Astpod <49484285+Astpod@users.noreply.github.com>
2021-07-28 00:14:26 +0300

    Update like.js

--- commands/like.js
+++ commands/like.js
@@ -14,7 +14,7 @@ module.exports.execute = async (client, message, args, embed) => {
       let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
       if(!member) return message.channel.send(embed.setDescription(`Bir kullanıcı etiketlemelisin.`))
       await LikeData.findOneAndUpdate({ guildID: message.guild.id, userID: member.user.id }, { $inc: { like: 1 } }, { upsert: true });
-      message.channel.send(embed.setDescription(`${message.author}, tarafından ${member} adlı kişiye like atıldı. \n kişinin günlük, toplam like bilgisi için **${cfg.prefix}like-bilgi** yazarak bakabilirsiniz.`)).then(x => x.delete({timeout: 9000}))
+      message.channel.send(embed.setDescription(`${message.author}, tarafından ${member} adlı kişiye like atıldı. \n kişinin toplam like sayısı için **${cfg.prefix}like-bilgi** yazarak bakabilirsiniz.`)).then(x => x.delete({timeout: 9000}))
       message.react(cfg.ok)
     }
   })
